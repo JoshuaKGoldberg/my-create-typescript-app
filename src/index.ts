de30ebb2d22b6c054f57de3d877fcae6d0df038e
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-03-02 13:38:26 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { template } from "./template.js";
+
+export { template as default, template };
+
+export * from "./base.js";
+export * from "./blocks/index.js";
+export * from "./presets/index.js";
